3da2a4831c8a8560ab7466c0fc655334cc66dd83
mariaramirezferro <maria.camilarami97@gmail.com>
2022-06-24 14:25:56 +0100

    Query for get the number of jobs completed by locksmith

---
+++ queries/TS_completed_jobs_by_locksmith.sql
@@ -0,0 +1,30 @@
+------------- Today's completed jobs by locksmiths 
+SELECT
+SB.LocksmithName AS "Locksmith",
+COUNT(*) AS "Completed jobs"
+FROM 
+(
+SELECT
+LD.*,
+LS.LocksmithName,
+PF.NetCost
+FROM [dbo].[Policy_LocksmithDetails] LD
+LEFT JOIN [dbo].[Lookup_Locksmiths] LS
+ON LD.LocksmithID = LS.ID
+LEFT JOIN [dbo].[Policy_Financial] PF
+ON LD.ReportID = PF.ReportID
+WHERE LD.Selected = 1
+AND LS.LocksmithName LIKE ('WGTK%')
+AND LD.ReportID IN (
+	SELECT
+	DISTINCT(PD.ReportID)
+	FROM
+	[dbo].[Policy_Diary] PD
+	WHERE PD.Active = 0
+	AND CAST(PD.ClosedDate AS DATE) = CAST(GETDATE() AS DATE)
+)
+AND CAST(LD.AvailableFromDate AS DATE) = CAST(GETDATE() AS DATE)
+) AS SB
+WHERE SB.NetCost IS NOT NULL
+GROUP BY SB.LocksmithName
+ORDER BY COUNT(*) DESC
