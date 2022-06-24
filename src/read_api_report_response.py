ffcf54a198c9e11c60ca1ad1a1bc290fbf4fc16d
felipemeloc <felipemeloc@gmail.com>
2022-06-24 14:22:29 +0100

    Use the same method to clean locksmiths names

--- src/read_api_report_response.py
+++ src/read_api_report_response.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import src.utils as utils
 
 
 URL= "https://track.roedan.com/api/generate_report?user_api_hash=%242y%2410%241bRB9V2n0RIU2LmzQ8BUMelSOXIoUvh.SdCMCSnCMPPmyZDzBZA6S&lang=en&format=html&type=39&date_from=2022-06-17+00%3A00%3A00&date_to=2022-06-18+00%3A00%3A00&devices%5B0%5D=51&devices%5B1%5D=72&devices%5B2%5D=26&devices%5B3%5D=93&devices%5B4%5D=55&devices%5B5%5D=95&devices%5B6%5D=57&devices%5B7%5D=11&devices%5B8%5D=19&devices%5B9%5D=80&devices%5B10%5D=24&devices%5B11%5D=96&devices%5B12%5D=16&devices%5B13%5D=54&devices%5B14%5D=58&devices%5B15%5D=98&devices%5B16%5D=94&devices%5B17%5D=21&devices%5B18%5D=89&devices%5B19%5D=52&devices%5B20%5D=65&devices%5B21%5D=107&devices%5B22%5D=68&devices%5B23%5D=17&stops=300&send_to_email=&generate=1"
@@ -39,7 +40,8 @@ def clean_report(df):
         df[col] = pd.to_datetime(df[col])
     for col in ['Duration', 'Time at location']:
         df[col] = pd.to_timedelta(df[col])
-    df['locksmith'] = df['locksmith'].str.lower().replace(r'wgtk[\s]*[\-]*[\s]*', '', regex=True).str.capitalize()
+    # df['locksmith'] = df['locksmith'].str.lower().replace(r'wgtk[\s]*[\-]*[\s]*', '', regex=True).str.capitalize()
+    df['locksmith'] = utils.clean_locksmith_name(df['locksmith'])
     return df
 
 if __name__ == '__main__':
