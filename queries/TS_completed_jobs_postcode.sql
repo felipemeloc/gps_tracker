7ccd0574ffea86b57cf12c14577da883fc26a96d
mariaramirezferro <maria.camilarami97@gmail.com>
2022-06-30 12:41:35 +0100

    Add information about the car model in the query

--- queries/TS_completed_jobs_postcode.sql
+++ queries/TS_completed_jobs_postcode.sql
@@ -3,19 +3,28 @@ SELECT
 SB.RecipientName AS "Locksmith",
 SB.LocksmithPostCode,
 SB.LocksmithSuppliedServicesIds,
+SB.Make AS "CarBrand",
+SB.Model AS "CarModel",
+SB.yearOfManufacture AS "CarYear",
 SB.ReportID
 FROM 
 (
 SELECT
-LD.*,
+DISTINCT(LD.ReportID),
+LD.LocksmithSuppliedServicesIds,
 CK.LocksmithPostCode,
 PF.RecipientName,
-PF.NetCost
+PF.NetCost,
+KC.Make,
+KC.Model,
+KC.yearOfManufacture
 FROM [dbo].[Policy_LocksmithDetails] LD
 LEFT JOIN [dbo].[Policy_Financial] PF
 ON LD.ReportID = PF.ReportID
 LEFT JOIN [dbo].[Policy_ClaimDetails_Key] CK
 ON LD.ReportID = CK.ReportID
+LEFT JOIN [dbo].[Policy_KeyClaims] KC
+ON LD.ReportID = KC.ReportID
 WHERE LD.Selected = 1
 AND PF.RecipientName LIKE ('WGTK%')
 AND LD.ReportID IN (
@@ -29,4 +38,4 @@ AND LD.ReportID IN (
 AND CAST(LD.AvailableFromDate AS DATE) = CAST(GETDATE() AS DATE)
 ) AS SB
 WHERE SB.NetCost IS NOT NULL
-GROUP BY SB.RecipientName, SB.LocksmithPostCode, SB.LocksmithSuppliedServicesIds, SB.ReportID;
+GROUP BY SB.RecipientName, SB.LocksmithPostCode, SB.LocksmithSuppliedServicesIds, SB.Make, SB.Model, SB.yearOfManufacture, SB.ReportID;
