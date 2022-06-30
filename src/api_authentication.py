13221ebd0c01c78e072def5cbffda5cd00af3dd0
mariaramirezferro <maria.camilarami97@gmail.com>
2022-06-30 18:24:42 +0100

    Functions documentation

--- src/api_authentication.py
+++ src/api_authentication.py
@@ -1,15 +1,35 @@
+"""api_authentication.py
+
+Contains the following function:
+* authentication: Returns a dictionary with the message response from the authentication to the Rodean API. use:
+    import api_authentication
+    api_authentication.authentication()
+
+"""
+
 import os
 import requests
 from dotenv import load_dotenv
 
-
+# Load environment variables
 load_dotenv()
 
 EMAIL = os.getenv('EMAIL')
 PASSWORD = os.getenv('PASSWORD')
 AUTHENTICATION_URL = os.getenv('AUTHENTICATION_URL')
 
-def authentication():
+def authentication()->dict:
+    """Function for authentication inside the Rodean API. The function uses the credentias used for Rodean."
+
+    Returns:
+        dict: dictionary with the following structure:
+        {
+            status_code: server response (3 digit integer)
+            USER_API_HASH: encrypted key for access the API endpoints
+            message: in case of error, problem description
+            process: name of the process carried out (for this case is AUTHENTICATION)
+        }
+    """
     credentials = {'email': EMAIL,
                'password': PASSWORD}
 
@@ -18,6 +38,7 @@ def authentication():
     #print(f'AUTHENTICATION: STATUS CODE {status_code}')
     USER_API_HASH = None
     message = None
+    # When the status code is 200, the connection was successfull
     if status_code == 200:
         USER_API_HASH = api_response.json()['user_api_hash']
     else:
