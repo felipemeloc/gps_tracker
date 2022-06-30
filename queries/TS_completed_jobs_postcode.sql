9797b34fb80a473f2cfc990f8489db0f135e5965
mariaramirezferro <maria.camilarami97@gmail.com>
2022-06-30 16:52:55 +0100

    Custom query to use any date as a filter

--- queries/TS_completed_jobs_postcode.sql
+++ queries/TS_completed_jobs_postcode.sql
@@ -2,29 +2,19 @@
 SELECT
 SB.RecipientName AS "Locksmith",
 SB.LocksmithPostCode,
-SB.LocksmithSuppliedServicesIds,
-SB.Make AS "CarBrand",
-SB.Model AS "CarModel",
-SB.yearOfManufacture AS "CarYear",
 SB.ReportID
 FROM 
 (
 SELECT
 DISTINCT(LD.ReportID),
-LD.LocksmithSuppliedServicesIds,
 CK.LocksmithPostCode,
 PF.RecipientName,
-PF.NetCost,
-KC.Make,
-KC.Model,
-KC.yearOfManufacture
+PF.NetCost
 FROM [dbo].[Policy_LocksmithDetails] LD
 LEFT JOIN [dbo].[Policy_Financial] PF
 ON LD.ReportID = PF.ReportID
 LEFT JOIN [dbo].[Policy_ClaimDetails_Key] CK
 ON LD.ReportID = CK.ReportID
-LEFT JOIN [dbo].[Policy_KeyClaims] KC
-ON LD.ReportID = KC.ReportID
 WHERE LD.Selected = 1
 AND PF.RecipientName LIKE ('WGTK%')
 AND LD.ReportID IN (
@@ -33,9 +23,9 @@ AND LD.ReportID IN (
 	FROM
 	[dbo].[Policy_Diary] PD
 	WHERE PD.Active = 0
-	AND CAST(PD.ClosedDate AS DATE) = CAST(GETDATE() AS DATE)
+	AND CAST(PD.ClosedDate AS DATE) = '{TARGET_DATE}'
 )
-AND CAST(LD.AvailableFromDate AS DATE) = CAST(GETDATE() AS DATE)
+AND CAST(LD.AvailableFromDate AS DATE) = '{TARGET_DATE}'
 ) AS SB
 WHERE SB.NetCost IS NOT NULL
-GROUP BY SB.RecipientName, SB.LocksmithPostCode, SB.LocksmithSuppliedServicesIds, SB.Make, SB.Model, SB.yearOfManufacture, SB.ReportID;
+GROUP BY SB.RecipientName, SB.LocksmithPostCode, SB.ReportID;
