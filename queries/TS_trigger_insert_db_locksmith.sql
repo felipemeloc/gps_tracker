e7bd2f8dec43dc071a5ecf7b3b6d65030599890e
mariaramirezferro <maria.camilarami97@gmail.com>
2022-07-07 10:22:22 +0100

    Add the drop if exist statement in the trigger

--- queries/TS_trigger_insert_db_locksmith.sql
+++ queries/TS_trigger_insert_db_locksmith.sql
@@ -1,20 +1,21 @@
 ------------- Trigger before insert into the gps rodean table
-CREATE TRIGGER update_rodean_locksmith ON [dbo].[gps_rodean_locksmith]
+CREATE OR ALTER TRIGGER update_rodean_locksmith ON [dbo].[gps_roedan_locksmith]
 	INSTEAD OF INSERT
 	AS
 	BEGIN
+	DROP TABLE IF EXISTS #TempRoedam;
 	SELECT * INTO #TempRoedam FROM INSERTED;
 
-	DELETE FROM [dbo].[gps_rodean_locksmith]
+	DELETE FROM [dbo].[gps_roedan_locksmith]
 	WHERE Route_ID IN (
 		SELECT DISTINCT(gs.Route_ID)
-		FROM [dbo].[gps_rodean_locksmith] gs
+		FROM [dbo].[gps_roedan_locksmith] gs
 		INNER JOIN #TempRoedam tm
 		ON gs.Route_ID = tm.Route_ID
 		WHERE gs.Route_ID = tm.Route_ID
 	) ;
 
-	INSERT INTO [dbo].[gps_rodean_locksmith]
+	INSERT INTO [dbo].[gps_roedan_locksmith]
 	SELECT * FROM #TempRoedam;
 	
 	END
